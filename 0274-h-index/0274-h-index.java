06a5e1e360be0b7c3c2229e83c4645d3e535250d
Irina0007 <sw6431@nyu.edu>
2026-07-07 17:05:57 +0800

    Time: 0 ms (100%), Space: 43.3 MB (84.6%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        int res=0;
+       int[] nc=new int[n+1];
+       for(int i=0;i<n;i++){
+        int a=Math.min(n,citations[i]);
+        nc[a]++;
+       }
+       for(int j=n;j>0;j--){
+        res=res+nc[j];
+        if(res>=j){
+            return j;
+        }
+
+       }
+    return 0;
+    }
+
+}
