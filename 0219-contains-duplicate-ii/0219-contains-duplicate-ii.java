7a8ca83321647dbcf739eae651b6852ca88552d1
Irina0007 <sw6431@nyu.edu>
2026-07-11 23:51:14 +0800

    Time: 23 ms (79.08%), Space: 90.9 MB (10.99%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            if(map.containsKey(a)){
+                if(i-map.get(a)<=k){
+                    return true;
+                }
+            } map.put(a,i);
+        }
+        return false;
+    }
+}
