db07744c55bfacaf7551088bb266419f8a3f83f0
Irina0007 <sw6431@nyu.edu>
2026-07-12 22:54:44 +0800

    Time: 2 ms (72.55%), Space: 43 MB (25.84%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length()){
+            return false;
+        }
+        else{
+            int i=0;
+            for (int j=0;j<t.length();j++){
+                if(i<s.length()&&t.charAt(j)==s.charAt(i)){
+                    i++;
+                }
+            }
+            return i==s.length();
+                       
+        }
+    }
+}
