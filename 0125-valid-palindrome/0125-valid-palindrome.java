aaef2b27e0e85a6934a81143f62adb57497be282
Irina0007 <sw6431@nyu.edu>
2026-07-14 18:26:02 +0800

    Time: 2 ms (99.28%), Space: 44 MB (92.08%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l=0;
+        int r=s.length()-1; 
+        while(l<r){
+            while(l<r&&!isNumApha(s.charAt(l))){
+            l++;
+            }while(l<r&&!isNumApha(s.charAt(r))){
+              r--;
+            }
+            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+                return false;
+            }
+            l++;
+            r--;
+         }
+     return true;
+    }
+     private boolean isNumApha(char c){
+        if(c >= 'A' && c <= 'Z' ||
+            c >= 'a' && c <= 'z' ||
+            c >= '0' && c <= '9'){
+            return true;
+            }
+         else{
+         return false;
+        }
+    }
+}
