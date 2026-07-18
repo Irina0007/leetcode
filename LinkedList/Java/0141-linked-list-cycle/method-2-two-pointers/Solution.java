aa46eb85648c35232f596a2d34e79f8f106fd674
irinawang <sw6431@nyu.edu>
2026-07-18 17:30:16 +0800

    Add two pointer method for linked list cycle

--- LinkedList/Java/0141-linked-list-cycle/method-2-two-pointers/Solution.java
+++ LinkedList/Java/0141-linked-list-cycle/method-2-two-pointers/Solution.java
@@ -11,14 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast == slow) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
         }
+
         return false;
-       
     }
 }
