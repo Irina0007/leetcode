1fd1e03f40c9deacd858b85395c6b52094c1bcd2
Irina0007 <sw6431@nyu.edu>
2026-08-03 14:20:52 +0800

    Time: 2 ms (12.73%), Space: 45.1 MB (51.22%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list=new ArrayList<>();
+    public boolean isValidBST(TreeNode root) {
+        Inorder(root);
+        int size=list.size();
+        for(int i=1;i<size;i++){
+            if(list.get(i)<=list.get(i-1)){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+    public void Inorder(TreeNode root){
+        if(root==null){
+            return;
+        }
+         Inorder(root.left);
+         list.add(root.val);
+         Inorder(root.right);
+    }
+}
